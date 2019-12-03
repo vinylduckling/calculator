588b791eb7af17e2c0f26f63dd3509474d0180d3
Lydia Wang <lydia@vinylduckling.com>
2019-12-02 19:04:07 -0500

    starting files and very basic html layout

---
+++ script.js
@@ -0,0 +1,44 @@
+
+
+
+
+
+
+
+
+
+function operate(operator, a, b) {
+    switch(operator) {
+        case '+':
+            add(a, b);
+            break;
+        case '-':
+            subtract(a, b);
+            break;
+        case '*':
+            multiply(a, b);
+            break;
+        case '/':
+            divide(a, b);
+            break;
+        default:
+            console.log('Operator does not match +, -, *, /') 
+    }
+}
+
+
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    return a / b;
+}
